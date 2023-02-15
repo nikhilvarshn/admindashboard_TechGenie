4fea53e18247d3590f636d0e15369c6ea7151d77
Nihkil Varshney <nikhilvarshney.0508@gmail.com>
2023-02-15 16:49:44 +0530

    active inactive users

--- resources/views/registered.blade.php
+++ resources/views/registered.blade.php
@@ -9,40 +9,32 @@
             <div class="card">
                 <div class="card-body">
                     <h4 style="display: inline;">Registered</h4>
-                    {{-- <button style="display: inline;float:right;" class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target=".bd-example-modal-lg">Add New Qualification</button> --}}
+                    <button style="display: inline;float:right;" class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target=".bd-example-modal-lg">Add New Qualification</button>
                 </div>
             </div> 
-        
             <div class="card">
                 <div class="card-body">
                     <div class="table-responsive">
                         <table class="display" id="data-table">
                             <thead>
                                 <tr>
-                                    <th>USER ID</th>
-                                    <th>Full Name </th>
+                                    <th>Sr.No</th>
+                                    <th>Name</th>
                                     <th>Email</th>
+                                    {{-- <th>Updated At</th> --}}
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach($users as $value)
-                                <tr>
-                                    <td>USER-{{ $value->id }}</td>
-                                    <td>{{ $value->full_name }}</td>
-                                    <td>{{ $value->email }}</td>
-                                </tr>
-                                @endforeach
                             </tbody>
                         </table>
                     </div>
                 </div>
-            </div> 
+            </div>
         </div>
     </div>
     {{-- @include('modals/qualification_addedit') --}}
     <!-- Container-fluid Ends-->
 </div>
-
 @endsection
 
 @section('script')
@@ -50,12 +42,14 @@
         var table = $('#data-table').DataTable({
             processing: true,
             serverSide: true,
-            // ajax: "{{ url('getQualificationData') }}",
+            ajax: "{{ url('/getMentorData') }}",
             columns: [
                 {data: 'DT_RowIndex', name: 'DT_RowIndex'},
                 {data: 'full_name', name: 'full_name'},
                 {data: 'email', name: 'email'},
                 // {data: 'status', name: 'status', orderable: false, searchable: false},
+                // {data: 'updated_at', name: 'Last Update', orderable: false, searchable: false},
+                // {data: 'action', name: 'action', orderable: false, searchable: false},
             ],
 
             language: {
@@ -64,6 +58,5 @@
             }
 		});
 </script>
-
 
 @endsection
